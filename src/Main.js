35159bcf92e98e351ab6034df6f86f4d88af7c8a
esitorsunil <sunilrio23@gmail.com>
2024-11-28 23:53:44 +0530

    update css

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,31 @@
 import React from 'react'
 
 const Main = () => {
+  const integrients = ["Chicken", "Oragano", "Tomatoes"];
+
+  const integrientsListItems = integrients.map((integrient) => ((
+    <li key={integrient}>{integrient}</li>
+  )));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIntegrient = formData.get("integrient");
+    console.log(newIntegrient);
+  }
+
   return (
     <div className='main'>
-        <form className='add-integrients-form'>
+        <form onSubmit={handleSubmit} className='add-integrients-form'>
             <input 
             type='text'
             placeholder='e.g. oregano'
             aria-label='Add integrient'
+            name='integrient'
             />
             <button>Add integrient</button>
         </form>
+        <ul>{integrientsListItems}</ul>
     </div>
   )
 }
