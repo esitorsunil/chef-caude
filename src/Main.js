ab844d3ac93655cda4f4eb3af847587b8caa8aaa
esitorsunil <sunilrio23@gmail.com>
2024-11-30 00:38:55 +0530

    form update

--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,22 @@
 import React from 'react'
 
 const Main = () => {
-  const integrients = ["Chicken", "Oragano", "Tomatoes"];
+  const [integrients, setintegrients] = React.useState([]);
 
-  const integrientsListItems = integrients.map((integrient) => ((
+  const integrientsListItems = integrients.map(integrient => ((
     <li key={integrient}>{integrient}</li>
   )));
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
+  function addIntegrient(formData) {
     const newIntegrient = formData.get("integrient");
-    console.log(newIntegrient);
+    
+    setintegrients(prevIntegrients => [...prevIntegrients, newIntegrient]);
+    console.log(integrients);
   }
 
   return (
     <div className='main'>
-        <form onSubmit={handleSubmit} className='add-integrients-form'>
+        <form action={addIntegrient} className='add-integrients-form'>
             <input 
             type='text'
             placeholder='e.g. oregano'
